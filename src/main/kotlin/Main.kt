e2a06be52885fdffaeab38a1a7adb4e4b89131fa
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-12-21 22:35:53 +0700

    Solves day 3 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-import solutions.Day2Part2
-import solutions.Day1Part1
-import solutions.Day2Part1
-import solutions.Day1Part2
+import solutions.*
 import java.io.File
 
 
 fun main(args: Array<String>) {
-    val problemNumber = "2_2"
+    val problemNumber = "3_1"
     val input = File("inputs/$problemNumber.txt").readLines().map { it.trim() }.filter { it.isNotBlank() }
     val solver = when (problemNumber) {
         "1_1" -> Day1Part1()
         "1_2" -> Day1Part2()
         "2_1" -> Day2Part1()
         "2_2" -> Day2Part2()
+        "3_1" -> Day3Part1()
         else -> throw IllegalArgumentException("Undefined Problem Number")
     }
     solver.solve(input)
---
+++ src/main/kotlin/solutions/Day3Part1.kt
@@ -0,0 +1,27 @@
+package solutions
+
+class Day3Part1 : Solution {
+    override fun solve(inputs: List<String>) {
+        var gamma = ""
+        var epsilon = ""
+        val ones = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        var size = 0
+
+        for (input in inputs) {
+            size += 1
+            for ((i, c) in input.withIndex()) {
+                if (c == '1') ones[i] += 1
+            }
+        }
+
+        val half = size / 2
+        ones.forEachIndexed { index, i ->
+            gamma += if (i > half) "1" else "0"
+            epsilon += if (i < half) "1" else "0"
+        }
+        val gammaInt = Integer.parseInt(gamma, 2)
+        val epsilonInt = Integer.parseInt(epsilon, 2)
+        val consumption = gammaInt * epsilonInt
+        println("Result gamma: $gamma, epsilon: $epsilon, consumption: $consumption")
+    }
+}
